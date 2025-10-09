28c2937d3c50305b8f7f1fac67725490809bb55b
oshithaB <ictdilunena@gmail.com>
2025-10-10 00:50:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
       'http://localhost:5173',
-      'https://powerkey-frontend-k7eh.vercel.app' // your Vercel frontend
+      'https://powerkey-frontend-9yrt.vercel.app' // your Vercel frontend
     ];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
