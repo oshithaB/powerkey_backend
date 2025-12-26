11e0a8a998c06df8945889b97e7aea227498fbae
oshithaB <ictdilunena@gmail.com>
2025-12-26 18:35:00 +0530

    Change server to use environment PORT variable

--- app.js
+++ app.js
@@ -179,6 +179,9 @@ io.on("connection", (socket) => {
 });
 
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
